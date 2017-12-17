983b27e316847acc078b152763c56defcde93365
Laighlinne <mylastmogletter@gmail.com>
2017-12-18 06:32:18 +0900

    Add files via upload

---
+++ MyCharFolder/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MyCharFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openfolder = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.compress = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.charname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.server = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(530, 330);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://www.ff14.co.kr/login/login/?GSPath=90688CE52E1192414EA0FD276133DA3C&Retur" +
+        "nUrl=F56677026F789CCAE7D763AC0ED05C8F", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.openfolder,
+            this.compress,
+            this.path,
+            this.charname,
+            this.server});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 24;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(530, 330);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // openfolder
+            // 
+            this.openfolder.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.openfolder.FillWeight = 60F;
+            this.openfolder.HeaderText = "폴더열기";
+            this.openfolder.Name = "openfolder";
+            this.openfolder.ReadOnly = true;
+            this.openfolder.Width = 60;
+            // 
+            // compress
+            // 
+            this.compress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.compress.FillWeight = 60F;
+            this.compress.HeaderText = "압축";
+            this.compress.Name = "compress";
+            this.compress.ReadOnly = true;
+            this.compress.Visible = false;
+            this.compress.Width = 60;
+            // 
+            // path
+            // 
+            this.path.FillWeight = 200F;
+            this.path.HeaderText = "폴더이름";
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Width = 200;
+            // 
+            // charname
+            // 
+            this.charname.FillWeight = 120F;
+            this.charname.HeaderText = "캐릭터명";
+            this.charname.Name = "charname";
+            this.charname.ReadOnly = true;
+            this.charname.Width = 120;
+            // 
+            // server
+            // 
+            this.server.FillWeight = 60F;
+            this.server.HeaderText = "서버";
+            this.server.Name = "server";
+            this.server.ReadOnly = true;
+            this.server.Width = 60;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(530, 330);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.webBrowser1);
+            this.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "계정캐릭폴더찾아주는프로그램일까아닐까 알아맞춰보세요";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn openfolder;
+        private System.Windows.Forms.DataGridViewButtonColumn compress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn path;
+        private System.Windows.Forms.DataGridViewTextBoxColumn charname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn server;
+    }
+}
+
---
+++ MyCharFolder/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Diagnostics;
+
+namespace MyCharFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Url.ToString() == "https://www.ff14.co.kr/login/login/?GSPath=90688CE52E1192414EA0FD276133DA3C&ReturnUrl=F56677026F789CCAE7D763AC0ED05C8F")
+            {
+                HtmlElement he = webBrowser1.Document.CreateElement("script");
+                HtmlElement st = webBrowser1.Document.CreateElement("style");
+                he.InnerText = "function windowFixed() {$('#loginForm>fieldset>.inp_wrap').css({'position':'fixed', 'left':'0px', 'top':'0px', 'background':'#030D1D', 'margin':'0px', 'padding':'0px', 'width':'530px', 'height':'330px', 'color':'#FFF', 'display':'block', 'padding-top':'40px', 'padding-left':'40px'}); $('#loginForm>fieldset>.inp_wrap>.btn_login').css({'position':'absolute', 'top':'40px', 'right':'134px'});} $('#loginForm>fieldset>.inp_wrap input').css({'background':'#1D2634', 'color':'#FFF', 'border-color':'transparent'}); $('#loginForm>fieldset>.inp_wrap dt>label').css({'color':'#FFF'}); $('#memberID').attr('placeholder', '아이디를 입력하세요'); $('#passWord').attr('placeholder', '비밀번호를 입력하세요');";
+                st.InnerText = "#memberID::-ms-input-placeholder, #passWord::-ms-input-placeholder {color:#616871;}";
+                webBrowser1.Document.Body.AppendChild(st);
+                webBrowser1.Document.Body.AppendChild(he);
+                webBrowser1.Document.InvokeScript("windowFixed");
+            }
+            else if (webBrowser1.Url.ToString() == "https://www.ff14.co.kr/login/?GSPath=90688CE52E1192414EA0FD276133DA3C&ReturnUrl=F56677026F789CCAE7D763AC0ED05C8F")
+            {
+                webBrowser1.Navigate("https://www.ff14.co.kr/login/login/?GSPath=90688CE52E1192414EA0FD276133DA3C&ReturnUrl=F56677026F789CCAE7D763AC0ED05C8F");
+            }
+            else if (webBrowser1.Url.ToString() == "https://www.ff14.co.kr/shop/myshop")
+            {
+                dataGridView1.Visible = true;
+                webBrowser1.Navigate("https://www.ff14.co.kr/shop/home/detail/34");
+            }
+            else if (webBrowser1.Url.ToString() == "https://www.ff14.co.kr/shop/home/detail/34")
+            {
+                string item = webBrowser1.Document.GetElementById("addOption").OuterHtml;
+                webBrowser1.Visible = false;
+                //MessageBox.Show(item);
+                XmlDocument xd = new XmlDocument();
+                xd.LoadXml(item);
+
+                foreach(XmlElement xe in xd.SelectSingleNode("/*").ChildNodes)
+                {
+                    string[] i = xe.GetAttribute("value").Split('/');
+                    try
+                    {
+                        string serv = "모그리";
+                        if (i[1].StartsWith("KrCh"))
+                            serv = "초코보";
+                        else if (i[1].StartsWith("KrCa"))
+                            serv = "카벙클";
+
+                        long code = Convert.ToInt64(i[0]);
+
+                        string f = "FFXIV_CHR00" + code.ToString("X");
+                        string p = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "My Games\\FINAL FANTASY XIV - KOREA", f);
+                        string open = "";
+                        string zip = "";
+
+                        if (Directory.Exists(p))
+                        {
+                            open = "열기";
+                            zip = "압축";
+                        }
+
+
+                        dataGridView1.Rows.Add(new string[] { open, zip, f, i[2], serv });
+                        //listView1.Items.Add(new ListViewItem(new string[] { "FFXIV_CHR00"+code.ToString("X"), i[2], serv }));
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView d = (DataGridView)sender;
+            if (e.ColumnIndex == 0)
+            {
+                string p = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "My Games\\FINAL FANTASY XIV - KOREA", d.Rows[e.RowIndex].Cells[2].Value.ToString());
+
+                Console.WriteLine(p);
+
+                if (Directory.Exists(p))
+                {
+                    Process.Start(p);
+                }
+            }
+        }
+    }
+}
